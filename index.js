ccdb761b3ad80c51583e2cb38f3582f77bb09482
Jymmy Murillo <murillojymmy@gmail.com>
2022-04-05 10:33:06 -0500

    Fixed deployment error on Heroku

--- index.js
+++ index.js
@@ -6,18 +6,26 @@ const PORT = process.env.PORT || 3030;
 const app = express();
 
 const todoRoutes = require("./routes/todoRoutes");
-const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false };
+const connectionOptions = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false,
+};
 
 app.use(express.json());
 app.use(cors());
 
 //"mongodb://localhost/todolist"
-mongoose.connect("mongodb+srv://jymmymurillo:1054374327@cluster0.4gblr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", connectionOptions)
-    .then(() => console.log("Connected successfully"))
-    .catch((err) => console.error(err));
+mongoose
+  .connect(
+    "mongodb+srv://jymmymurillo:1054374327@cluster0.4gblr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    connectionOptions
+  )
+  .then(() => console.log("Connected successfully"))
+  .catch((err) => console.error(err));
 
 app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
-    console.log("The server is listening on port " + PORT);
+  console.log("The server is listening on port " + PORT);
 });
